6c02774112b63907fd0f9cd4478025745f2f4c2f
natanlves <natan.alves.dev@gmail.com>
2025-02-05 16:10:23 -0400

    home page

---
+++ mercado/resources/views/layout/template.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="{{ asset('js/template.js') }}"></script>
+        <link rel="stylesheet" href="{{ asset('css/template.css') }}">
+        <title>Mercado</title>
+    </head>
+    <body>
+        <header> 
+            
+            <nav class="navbar">
+                <div class="logo">Mercado</div>
+                <ul id="nav-links">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Mercado</a></li>
+                    <li><a href="">Celulares</a></li>
+                    <li><a href="">Eletrodomesticos</a></li>
+                </ul>
+                <div class="menu-toggle" onclick="toggleMenu()">☰</div>
+            </nav>
+        </header>
+        
+    </body>
+</html>
